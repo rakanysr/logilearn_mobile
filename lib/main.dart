d3a73326e68c0843fa919e9f33013b7315e76f12
RakaValrizqy <pendragonqword@gmail.com>
2025-10-28 20:59:54 +0700

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'view/home_view.dart';
+import 'view/login_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: const HomeView());
+    return MaterialApp(home: const LoginView());
   }
 }
