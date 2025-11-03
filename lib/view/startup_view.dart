5dab06fa7af82c6c295c5638b70678442a5410f0
RakaValrizqy <pendragonqword@gmail.com>
2025-11-03 22:28:22 +0700

    menambahkan animasi transisi dari startup ke page login

--- lib/view/startup_view.dart
+++ lib/view/startup_view.dart
@@ -1,4 +1,4 @@
-import 'dart:async'; // Penting untuk Timer/Future.delayed
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:logilearn/view/login_view.dart';
@@ -10,21 +10,65 @@ class StartupView extends StatefulWidget {
   _StartupViewState createState() => _StartupViewState();
 }
 
-class _StartupViewState extends State<StartupView> {
-  final Color _backgroundColor = Color(0xFF2977FF);
+class _StartupViewState extends State<StartupView> with SingleTickerProviderStateMixin {
+  final Color _backgroundColor = const Color(0xFF2977FF);
+  late AnimationController _controller;
+  late Animation<double> _fadeAnimation;
+  late Animation<double> _scaleAnimation;
 
   @override
   void initState() {
     super.initState();
-    _navigateToNextScreen();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1500),
+    );
+
+    _fadeAnimation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    );
+
+    _scaleAnimation = Tween<double>(begin: 0.8, end: 1.0).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.elasticOut),
+    );
+
+    _controller.forward();
+
+    Timer(const Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(_createRoute());
+    });
   }
 
-  void _navigateToNextScreen() {
-    Future.delayed(Duration(seconds: 3), () {
-      Navigator.of(
-        context,
-      ).pushReplacement(MaterialPageRoute(builder: (context) => LoginView()));
-    });
+  Route _createRoute() {
+    return PageRouteBuilder(
+      transitionDuration: const Duration(milliseconds: 600),
+      pageBuilder: (context, animation, secondaryAnimation) =>
+          const LoginView(),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        const begin = Offset(1.0, 0.0);
+        const end = Offset.zero;
+        final tween =
+            Tween(begin: begin, end: end).chain(CurveTween(curve: Curves.easeInOut));
+
+        final opacityTween = Tween<double>(begin: 0.0, end: 1.0);
+
+        return SlideTransition(
+          position: animation.drive(tween),
+          child: FadeTransition(
+            opacity: animation.drive(opacityTween),
+            child: child,
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
@@ -32,20 +76,26 @@ class _StartupViewState extends State<StartupView> {
     return Scaffold(
       backgroundColor: _backgroundColor,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('assets/images/Mascot buntung.png', width: 150),
-            SizedBox(height: 24),
-            Text(
-              'LogiLearn',
-              style: GoogleFonts.inter(
-                fontWeight: FontWeight.w800,
-                fontSize: 48,
-                color: Colors.white,
-              ),
+        child: FadeTransition(
+          opacity: _fadeAnimation,
+          child: ScaleTransition(
+            scale: _scaleAnimation,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset('assets/images/Mascot buntung.png', width: 150),
+                const SizedBox(height: 24),
+                Text(
+                  'LogiLearn',
+                  style: GoogleFonts.inter(
+                    fontWeight: FontWeight.w800,
+                    fontSize: 48,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
