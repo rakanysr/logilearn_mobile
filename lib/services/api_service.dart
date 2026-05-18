52b7210c428fad5658d0428243329e6b8a360c33
rakanysr <mrakan711@gmail.com>
2026-05-18 20:43:06 +0700

    Remove unused dart:io import
    Remove the unused `dart:io` import from lib/services/api_service.dart. This cleans up an unnecessary import, prevents analyzer warnings and avoids potential platform issues (e.g., web builds) caused by importing dart:io.

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
